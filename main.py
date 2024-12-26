a8aebb17056544702ef24c2108f9c12d04174acc
Henrique Furtado - Insert <carloshenrique.furtado@hotmail.com>
2024-12-26 10:31:13 -0300

    feat: Adicionado rota para fazer upload do arquivo

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+import io
+import openpyxl
+from fastapi import FastAPI, File, HTTPException, UploadFile
 
 app = FastAPI()
 
@@ -8,3 +10,18 @@ def hello_root():
         "message": "Hello World"
     }
 
+@app.post("/uploadFile/")
+async def create_upload_file(file: UploadFile):
+    if file.filename.endswith('.xlsx'):
+        f = await file.read()
+        xlsx = io.BytesIO(f)
+        wb = openpyxl.load_workbook(xlsx)
+        ws = wb['Sheet1']
+
+        for cells in ws.iter_rows():
+            print([cell.value for cell in cells])
+
+        return True
+    
+    else: 
+        raise HTTPException(status_code=400, detail="File must be in xlsx format")
